48086ff77d7f501a936dd34d8959596474100362
Irina Rodkina <roody2203@yandex.ru>
2025-04-01 21:42:30 +0300

    panel test was separated by parametrization

--- src/main/java/ru/praktikum/HomePage.java
+++ src/main/java/ru/praktikum/HomePage.java
@@ -20,54 +20,6 @@ public class HomePage {
     // Локатор выпадающего списка FAQ
     private final By dropDownList = By.className("Home_FAQ__3uVm4");
 
-    // Локатор 1-ой строки
-    private final By howMuchIsItAndHowPay = By.id("accordion__heading-0");
-
-    // Локатор выпадающего текста 1-ой строки
-    private final By howMuchIsItAndHowPayPanel = By.id("accordion__panel-0");
-
-    // Локатор 2-ой строки
-    private final By wantSeveralScootersAtOnce = By.id("accordion__heading-1");
-
-    // Локатор выпадающего текста 2-ой строки
-    private final By wantSeveralScootersAtOncePanel = By.id("accordion__panel-1");
-
-    // Локатор 3-ей строки
-    private final By howRentalTimeIsCalculated = By.id("accordion__heading-2");
-
-    // Локатор выпадающего текста 3-ей строки
-    private final By howRentalTimeIsCalculatedPanel = By.id("accordion__panel-2");
-
-    // Локатор 4-ой строки
-    private final By canIOrderAScooterForToday = By.id("accordion__heading-3");
-
-    // Локатор выпадающего текста 4-ой строки
-    private final By canIOrderAScooterForTodayPanel = By.id("accordion__panel-3");
-
-    // Локатор 5-ой строки
-    private final By isItPossibleToExtendTheOrder = By.id("accordion__heading-4");
-
-    // Локатор выпадающего текста 5-ой строки
-    private final By isItPossibleToExtendTheOrderPanel = By.id("accordion__panel-4");
-
-    // Локатор 6-ой строки
-    private final By areYouBringingTheCharger = By.id("accordion__heading-5");
-
-    // Локатор выпадающего текста 6-ой строки
-    private final By areYouBringingTheChargerPanel = By.id("accordion__panel-5");
-
-    // Локатор 7-ой строки
-    private final By isItPossibleToCancelTheOrder = By.id("accordion__heading-6");
-
-    // Локатор выпадающего текста 7-ой строки
-    private final By isItPossibleToCancelTheOrderPanel = By.id("accordion__panel-6");
-
-    // Локатор 8-ой строки
-    private final By liveAcrossTheMKAD = By.id("accordion__heading-7");
-
-    // Локатор выпадающего текста 8-ой строки
-    private final By liveAcrossTheMKADPanel = By.id("accordion__panel-7");
-
     // Кнопка Заказать в шапке страницы
     private final By orderButtonTop = By.className("Button_Button__ra12g");
 
@@ -77,6 +29,11 @@ public class HomePage {
     // Кнопка закрытия куки
     private final By closeCookieButton = By.id("rcc-confirm-button");
 
+    // Начало имени локатора строки
+    private final String headingNamePrefix = "accordion__heading-";
+
+    // Начало имени локатора выпадающего текста строки
+    private final String panelNamePrefix = "accordion__panel-";
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -114,23 +71,32 @@ public class HomePage {
         driver.findElement(orderButtonBottom).click();
     }
 
-    // Проверяет  текст выпадающих панелей в разделе FAQ
-    public void checkPanels() {
-        checkPanelText(howMuchIsItAndHowPay, howMuchIsItAndHowPayPanel, ReferenceData.HOW_MUCH_IS_IT_AND_HOW_PAY);
-        checkPanelText(wantSeveralScootersAtOnce, wantSeveralScootersAtOncePanel, ReferenceData.WANT_SEVERAL_SCOOTERS_AT_ONCE);
-        checkPanelText(howRentalTimeIsCalculated, howRentalTimeIsCalculatedPanel, ReferenceData.HOW_RENTAL_TIME_IS_CALCULATED);
-        checkPanelText(canIOrderAScooterForToday, canIOrderAScooterForTodayPanel, ReferenceData.CAN_I_ORDER_A_SCOOTER_FOR_TODAY);
-        checkPanelText(isItPossibleToExtendTheOrder, isItPossibleToExtendTheOrderPanel, ReferenceData.IS_IT_POSSIBLE_TO_EXTEND_THE_ORDER);
-        checkPanelText(areYouBringingTheCharger, areYouBringingTheChargerPanel, ReferenceData.ARE_YOU_BRINGING_THE_CHARGER);
-        checkPanelText(isItPossibleToCancelTheOrder, isItPossibleToCancelTheOrderPanel, ReferenceData.IS_IT_POSSIBLE_TO_CANCEL_THE_ORDER);
-        checkPanelText(liveAcrossTheMKAD, liveAcrossTheMKADPanel, ReferenceData.LIVE_ACROSS_THE_MKAD);
-    }
-
     // Проверяет текст в выпадающей панели
-    public void checkPanelText(By header, By panel, String expectedText) {
-        driver.findElement(header).click();
-        driverWaiter.until(ExpectedConditions.visibilityOfElementLocated(panel));
-        String panelText = textOfPanel(panel);
-        MatcherAssert.assertThat(expectedText, is(panelText));
+    public void checkPanel(int headingIndex, String expectedHeadingText, String expectedPanelText) {
+        // Формируем селектор для заголовка
+        String headingName = headingNamePrefix + String.valueOf(headingIndex);
+        // Формируем селектор для панели
+        String panelName = panelNamePrefix + String.valueOf(headingIndex);
+
+        // Создаем локатор для заголовка
+        By headingLocator = By.id(headingName);
+        // Создаем локатор для панели
+        By panelLocator = By.id(panelName);
+
+        // Ищем элемент заголовка
+        WebElement heading = driver.findElement(headingLocator);
+        // Проверяем текст заголовка
+        MatcherAssert.assertThat(heading.getText(), is(expectedHeadingText));
+
+        // Нажимаем на заголовок
+        heading.click();
+
+        // Дожидаемся загрузки панели
+        driverWaiter.until(ExpectedConditions.visibilityOfElementLocated(panelLocator));
+
+        // Ищем элемент панели
+        WebElement panel = driver.findElement(panelLocator);
+        // Проверяем текст панели
+        MatcherAssert.assertThat(panel.getText(), is(expectedPanelText));
     }
 }
--- src/test/java/ru/yandex/praktikum/ScooterFAQWebTest.java
+++ src/test/java/ru/yandex/praktikum/ScooterFAQWebTest.java
@@ -4,16 +4,45 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.praktikum.EnvConfig;
 
 import ru.praktikum.HomePage;
+import ru.praktikum.ReferenceData;
 
+import java.sql.Ref;
 import java.time.Duration;
-
+@RunWith(Parameterized.class)
 public class ScooterFAQWebTest extends BaseWebTest {
+    private final int headingIndex;
+    private final String headingText;
+    private final String panelText;
+
+    //
+    public ScooterFAQWebTest(int headingIndex, String headingText, String panelText) {
+        this.headingIndex = headingIndex;
+        this.headingText = headingText;
+        this.panelText = panelText;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: {1}")
+    public static Object[][] getPanelText() {
+        return new Object[][] {
+                {0, ReferenceData.HOW_MUCH_IS_IT_AND_HOW_PAY_HEADING, ReferenceData.HOW_MUCH_IS_IT_AND_HOW_PAY_PANEL},
+                {1, ReferenceData.WANT_SEVERAL_SCOOTERS_AT_ONCE_HEADING, ReferenceData.WANT_SEVERAL_SCOOTERS_AT_ONCE_PANEL},
+                {2, ReferenceData.HOW_RENTAL_TIME_IS_CALCULATED_HEADING, ReferenceData.HOW_RENTAL_TIME_IS_CALCULATED_PANEL},
+                {3, ReferenceData.CAN_I_ORDER_A_SCOOTER_FOR_TODAY_HEADING, ReferenceData.CAN_I_ORDER_A_SCOOTER_FOR_TODAY_PANEL},
+                {4, ReferenceData.IS_IT_POSSIBLE_TO_EXTEND_THE_ORDER_HEADING, ReferenceData.IS_IT_POSSIBLE_TO_EXTEND_THE_ORDER_PANEL},
+                {5, ReferenceData.ARE_YOU_BRINGING_THE_CHARGER_HEADING, ReferenceData.ARE_YOU_BRINGING_THE_CHARGER_PANEL},
+                {6, ReferenceData.IS_IT_POSSIBLE_TO_CANCEL_THE_ORDER_HEADING, ReferenceData.IS_IT_POSSIBLE_TO_CANCEL_THE_ORDER_PANEL},
+                {7, ReferenceData.LIVE_ACROSS_THE_MKAD_HEADING, ReferenceData.LIVE_ACROSS_THE_MKAD_PANEL}
+        };
+    }
 
     @Test
     // Тест текста FAQ в Chrome
@@ -31,7 +60,7 @@ public class ScooterFAQWebTest extends BaseWebTest {
         // Закрывает куки
         homePage.closeCookie();
 
-        // Проверяет текст панелей
-        homePage.checkPanels();
+        // Проверяет текст панели
+        homePage.checkPanel(headingIndex, headingText, panelText);
     }
 }
